f2d3158afe2b56315a524aa6b6dff4c604742a7a
Garo Yeriazarian <garo.yeriazarian@gmail.com>
2019-12-17 16:39:34 -0600

    Setup hosted service, logging, and reasonable output

--- src/SuperTool/Program.cs
+++ src/SuperTool/Program.cs
@@ -1,12 +1,28 @@
 ﻿namespace SuperTool
 {
-    using System;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.DependencyInjection;
+    using Microsoft.Extensions.Hosting;
 
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            using var host = CreateHostBuilder(args).Build();
+            await host.StartAsync();
+            await host.StopAsync();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<SuperToolHostedService>();
+
+                    // turn off the startup messages logged that we won't be using
+                    services.PostConfigure<ConsoleLifetimeOptions>(options => options.SuppressStatusMessages = true);
+                });
         }
     }
 }
---
+++ src/SuperTool/SuperToolHostedService.cs
@@ -0,0 +1,30 @@
+﻿namespace SuperTool
+{
+    using System;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Extensions.Hosting;
+    using Microsoft.Extensions.Logging;
+
+    public class SuperToolHostedService : IHostedService
+    {
+        private readonly ILogger<SuperToolHostedService> _logger;
+
+        public SuperToolHostedService(ILogger<SuperToolHostedService> logger)
+        {
+            _logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Hi from the command line!");
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Bye from the command line!");
+            return Task.CompletedTask;
+        }
+    }
+}
