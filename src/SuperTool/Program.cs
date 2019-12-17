dbefd2a8ca13effb3e05823e344551db12990cdf
Garo Yeriazarian <garo.yeriazarian@gmail.com>
2019-12-17 16:10:30 -0600

    Add empty console project

---
+++ src/SuperTool/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace SuperTool
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
